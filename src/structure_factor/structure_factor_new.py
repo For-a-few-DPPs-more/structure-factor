a06aa6b0d4035be9e6d8f56acfffcb0a549568c5
Guillaume Gautier <guillaume.gga@gmail.com>
2021-06-08 09:37:38 +0200

    Propose a simpler structure factor class

---
+++ src/structure_factor/structure_factor_new.py
@@ -0,0 +1,156 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from rpy2.robjects import numpy2ri
+
+from structure_factor.utils import compute_scattering_intensity
+from structure_factor.transforms import RadiallySymmetricFourierTransform
+from structure_factor.spatstat_interface import SpatstatInterface
+
+
+class StructureFactor:
+    """Implementation of various estimators of the structure factor of a 2 dimensional point process.
+    # todo add link to definition
+    """
+
+    def __init__(self, points, intensity):
+        r"""
+        Args:
+            points: :math:`n \times 2` np.array representing a realization of a 2 dimensional point process.
+            intensity: intensity of the underlying point process.
+
+        # todo treat the case where the intensity is not provided
+        # todo consider passing the window where the points were observed to avoid raidus in compute_pcf(self, radius...
+        """
+        dimension = points.shape[1]
+        assert points.ndim == 2 and dimension == 2
+        self.dimension = dimension
+        self.points = points
+        self.intensity = intensity
+
+    def compute_scattering_intensity(self, L, maximum_wave, meshgrid_size=None):
+        # todo replace the link below to the link of our future paper :).
+        r"""Compute the ensemble estimator of the scattering intensity described in http://www.scoste.fr/survey_hyperuniformity.pdf.(equation 4.5).
+
+        .. math::
+
+            SI(k) =
+            \left\lvert
+                \sum_{x \in \mathcal{X}}
+                    \exp(- i \left\langle k, x \right\rangle)
+            \right\rvert^2
+
+        Note: This estimation converges to the structure factor in the thermodynamic limits.
+
+        Notes:  The data should be simulated inside a cube. # todo what is data ?
+                The allowed values of wave vectors are the points of the dual of the lattice having fundamental cell the cubic window .
+                This is represented inside wave_vectors defined as :
+                wave_vectors = 2*pi*k_vector/L where, k_vector is a vector of integer from 1 into maximum_k, and L in the length side of the cubic window.
+
+        Args:
+            L (int): length of the square that contains the data.
+            maximum_wave (int): maximum of wave vector
+            meshgrid_size (int): if the requested evaluation is on a meshgrid,  then meshgrid_size is the number of wave vector in each row of the meshgrid. Defaults to None.
+
+        Returns:
+            :math:`\left\lVert k \right\rVert, SI(K)`, the norm of the wave vectors :math:`k` and the estimation of the scattering intensity evaluated at :math:`k`.
+        """
+        maximum_k = np.floor(maximum_wave * L / (2 * np.pi * np.sqrt(2)))
+        if meshgrid_size is None:
+            x = np.linspace(1, maximum_k, int(maximum_k))
+            k_ = np.column_stack((x, x))
+        else:
+            x_grid = np.linspace(0, maximum_k, int(meshgrid_size))
+            X, Y = np.meshgrid(x_grid, x_grid)
+            k_ = np.column_stack((X.ravel(), Y.ravel()))
+
+        k_norm = np.linalg.norm(k_, axis=1)
+        si = compute_scattering_intensity(k_, self.points)
+
+        if meshgrid_size is not None:
+            k_norm = k_norm.reshape(X.shape)
+            si = si.reshape(X.shape)
+
+        return k_norm, si
+
+    def compute_pcf(self, radius, method, install_spatstat=False, **params):
+        """Estimate the pair correlation function of ``self.points`` observed in a disk window centered at the origin with radius ``radius`` using spatstat ``spastat.core.pcf_ppp`` or ``spastat.core.pcf_fv`` functions according to ``method`` with the corresponding parameters ``params``.
+
+        # todo consider adding the window where points were observed at __init__ to avoid radius argument.
+        radius:
+        method: "ppp" or "fv" referring to ``spastat.core.pcf.ppp`` or ``spastat.core.pcf.fv`` functions for estimating the pair correlation function.
+        install_spatstat: [description], defaults to False
+        params:
+            - method = 'ppp'
+                - dict(``spastat.core.pcf.ppp`` parameters)
+            - method = 'fv'
+                - dict(
+                    Kest=dict(``spastat.core.Kest`` parameters),
+                    fv=dict(``spastat.core.pcf.fv`` parameters)
+                )
+
+        Return dict representing the DataFrame output of `spastat.core.pcf.ppp`` or ``spastat.core.pcf.fv`` functions.
+
+        .. seealso::
+
+            - `pcf.ppp <https://www.rdocumentation.org/packages/spatstat.core/versions/2.1-2/topics/pcf.ppp>`_
+            - `pcf.fv <https://www.rdocumentation.org/packages/spatstat.core/versions/2.1-2/topics/pcf.fv>`_
+        """
+        assert method in ("ppp", "fv")
+
+        # core, geom and other subpackages are updated if install_spatstat
+        spatstat = SpatstatInterface(update=install_spatstat)
+        spatstat.import_package("core", "geom", update=False)
+
+        window = spatstat.geom.disc(radius)
+
+        numpy2ri.activate()
+        data = spatstat.geom.ppp(*self.points.T, window=window)
+        numpy2ri.deactivate()
+
+        if method == "ppp":
+            pcf = spatstat.core.pcf_ppp(data, **params)
+
+        if method == "fv":
+            params_Kest = params.get("Kest", dict())
+            k_ripley = spatstat.core.Kest(data, **params_Kest)
+            params_fv = params.get("fv", dict())
+            pcf = spatstat.core.pcf_fv(k_ripley, **params_fv)
+
+        return dict(zip(pcf.names, np.asarray(pcf)))
+
+    def interpolate_pair_correlation_function(self, r, pcf_r, **params):
+        """Interpole given evaluations of the pair correlation function (g)Returns an interpolation of the total correlation function (h=g-1)
+
+        Args:
+            r: vector containing the radius on which the pair correlation function is evaluated.
+            pcf_r: vector containing the evaluations of the pair correlation function on r_vec.
+            params: dict of the parameters of :py:func:`scipy.interpolate.interp1d` function.
+
+        # todo clarify whether is it F[g] or F[g-1]
+        """
+
+        return interpolate.interp1d(r, pcf_r - 1.0, **params)
+
+    def compute_structure_factor(self, k, pcf, method="Ogata", **params):
+        r"""Compute the structure factor of the underlying point process
+        .. math::
+
+            SF(k) = 1 + \rho F[g](k)
+
+        where
+        - :math:`\rho` is the intensity of the point process ``self.intensity``
+        - :math:`g` is the corresponding radially symmetric pair correlation function ``pcf``.
+
+        .. note::
+
+            Typical usage: ``pcf`` is estimated using :py:meth:`StructureFactor.compute_pcf` and then interpolated using :py:meth:`StructureFactor.interpolate_pair_correlation_function`.
+
+        # todo clarify whether is it F[g] or F[g-1]
+        """
+        assert callable(pcf)
+        d = self.dimension
+        rho = self.intensity
+        ft = RadiallySymmetricFourierTransform(dimension=d)
+        ft_k = ft.transform(pcf, k, method=method, **params)
+        return 1.0 + rho * ft_k
