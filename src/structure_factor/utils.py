711f99d68aa1e9a2ac9fc86822cffc77d3626f78
Guillaume Gautier <guillaume.gga@gmail.com>
2021-07-21 18:41:20 +0200

    Clean up utils file

--- src/structure_factor/utils.py
+++ src/structure_factor/utils.py
@@ -9,9 +9,6 @@ from scipy.special import j0, j1, jv, jn_zeros, y0, y1, yv
 from scipy import interpolate, stats
 import matplotlib.pyplot as plt
 
-# todo bien renomer les variables
-# todo clean up the file: remove unused utility functions like get_x, roots etc
-
 
 def cleaning_data(data):
     data_clean = np.nan_to_num(data, nan=0, posinf=0, neginf=0)
@@ -20,13 +17,6 @@ def cleaning_data(data):
 
 def get_random_number_generator(seed):
     """Turn seed into a np.random.Generator instance"""
-    if isinstance(seed, np.random.Generator):
-        return seed
-    if seed is None or isinstance(seed, (int, np.integer)):
-        return np.random.default_rng(seed)
-    raise TypeError(
-        "seed must be None, an np.random.Generator or an integer (int, np.integer)"
-    )
     return np.random.default_rng(seed)
 
 
@@ -48,56 +38,6 @@ def bessel2(order, x):
     if order == 1:
         return y1(x)
     return yv(order, x)
-
-
-def roots(d, N):
-    # first N Roots of the Bessel J_(d/2-1) functions divided by pi.
-    return np.array([mpm.besseljzero(d / 2 - 1, i + 1) for i in range(N)]) / np.pi
-
-
-def get_x(h, zeros):
-    return np.pi * psi(h * zeros) / h
-
-
-def weight(d, zeros):
-    return bessel2(d / 2 - 1, np.pi * zeros) / bessel1(d / 2, np.pi * zeros)
-
-
-def psi(t):
-    # equation 5.1 Ogata https://www.kurims.kyoto-u.ac.jp/~okamoto/paper/Publ_RIMS_DE/41-4-40.pdf
-    return t * np.tanh((0.5 * np.pi) * np.sinh(t))
-
-
-def d_psi(t):
-    # equation 5.1 Ogata https://www.kurims.kyoto-u.ac.jp/~okamoto/paper/Publ_RIMS_DE/41-4-40.pdf
-    threshold = 3.5  # threshold outside of which psi' plateaus to -1, 1
-    out = np.sign(t)
-    mask = np.abs(t) < threshold
-    x = t[mask]
-    out[mask] = np.pi * x * np.cosh(x) + np.sinh(np.pi * np.sinh(x))
-    out[mask] /= 1.0 + np.cosh(np.pi * np.sinh(x))
-    return out
-
-
-def integrate_with_abs_odd_monomial(f, nu=0, h=0.1, n=100, f_even=False):
-    # Section 1 Ogata https://www.kurims.kyoto-u.ac.jp/~okamoto/paper/Publ_RIMS_DE/41-4-40.pdf
-    x = bessel1_zeros(nu, n)
-    weights = bessel2(nu, x) / bessel1(nu + 1, x)  # equation 1.2
-    x *= h / np.pi  # equivalent of xi variable
-    # equation 1.1
-    if f_even:
-        return 2.0 * h * np.sum(weights * np.power(x, 2 * nu + 1) * f(x))
-    return h * np.sum(weights * np.power(x, 2 * nu + 1) * (f(x) + f(-x)))
-
-
-def integrate_with_bessel_function_half_line(f, nu=0, h=0.01, n=1000):
-    # Section 5 Ogata https://www.kurims.kyoto-u.ac.jp/~okamoto/paper/Publ_RIMS_DE/41-4-40.pdf
-    t = bessel1_zeros(nu, n)
-    weights = bessel2(nu, t) / bessel1(nu + 1, t)  # equation 1.2
-    t *= h / np.pi  # equivalent of xi variable
-    # Change of variable equation 5.2
-    x = (np.pi / h) * psi(t)
-    return np.pi * np.sum(weights * f(x) * bessel1(nu, x) * d_psi(t))
 
 
 def compute_scattering_intensity(k, data):
@@ -122,26 +62,6 @@ def _binning_function(x, y, **params):
     bin_centers = bin_edges[:-1] + np.diff(bin_edges) / 2
 
     return bin_centers, bin_mean, bin_std
-
-
-# ! replaced by np.polyfit
-def _lsf(x_data, y_data, stop=None):
-    if stop is not None:
-        x = x_data[:stop]
-        y = y_data[:stop]
-    else:
-        x = x_data
-        y = y_data
-
-    N = x.shape[0]
-    x_square = x ** 2
-    xy = x * y
-    sum_x = np.sum(x)
-    sum_y = np.sum(y)
-    slope = (N * np.sum(xy) - sum_x * sum_y) / (N * np.sum(x_square) - sum_x ** 2)
-    y_intercept = (sum_y - slope * sum_x) / N
-    fitted_line = lambda t: slope * t + y_intercept
-    return fitted_line
 
 
 def plot_summary(x, y, axis, label="Mean", **binning_params):
