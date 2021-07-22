0fd8967c24b3e04ebd86d2a173f7df4de0bc2f26
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-07-22 13:15:52 +0200

    add atribut : fitted_line

--- src/hypton/effective_hyperuniform.py
+++ src/hypton/effective_hyperuniform.py
@@ -9,17 +9,19 @@ class EffectiveHyperuniform:
     # ! What is the purpose of this class ?
     # ! Can't it be reduced to simple function calls to _binning_function and index_H?
 
-    def __init__(self, k_norm, sf):
-        self.k_norm = k_norm
+    def __init__(self, norm_k, sf):
+        self.norm_k = norm_k
         self.sf = sf
 
     def bin_data(self, **params):
         # ! what is the purpose of this method ?
-        return _binning_function(self.k_norm, self.sf, **params)
+        return _binning_function(self.norm_k, self.sf, **params)
 
-    def index_H(self, k_norm, sf, i_max=None):
-        i = len(k_norm) if i_max is None else i_max
-        S_0 = np.polyfit(k_norm[:i], sf[:i], deg=1)[-1]
+    def index_H(self, norm_k, sf, i_max=None):
+        i = len(norm_k) if i_max is None else i_max
+        fitting_param = np.polyfit(norm_k[:i], sf[:i], deg=1)
+        S_0 = fitting_param[-1]
         thresh = 1
         S_first_peak = max(thresh, sf[np.argmax(sf > thresh)])
+        self.fitted_line = lambda x: fitting_param[0] * x + S_0
         return S_0 / S_first_peak
