ed72c9950bd459a9c34c0584fe716d87f72610e9
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-07-24 13:16:14 +0200

    change the stop to make it easier on the user

--- src/hypton/effective_hyperuniform.py
+++ src/hypton/effective_hyperuniform.py
@@ -18,11 +18,17 @@ class EffectiveHyperuniform:
         # ! what is the purpose of this method ?
         return _binning_function(self.norm_k.ravel(), self.sf.ravel(), **params)
 
-    def index_H(self, norm_k, sf, i_max=None):
-        i = len(norm_k) if i_max is None else i_max
+    def index_H(self, norm_k, sf, norm_k_stop=None):
+        if norm_k_stop is not None:
+            norm_k_list = list(norm_k.ravel())
+            index = min(
+                range(len(norm_k_list)), key=lambda i: abs(norm_k_list[i] - norm_k_stop)
+            )  # index of the closest value to k_stop in norm_k
+        i = len(norm_k) if norm_k_stop is None else index
         fitting_param = np.polyfit(norm_k[:i], sf[:i], deg=1)
         S_0 = fitting_param[-1]
         thresh = 1
         S_first_peak = max(thresh, sf[np.argmax(sf > thresh)])
+        print(S_first_peak)
         self.fitted_line = lambda x: fitting_param[0] * x + S_0
         return S_0 / S_first_peak
