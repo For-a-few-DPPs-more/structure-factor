5e1bfa3acfb960a82b66a11fba46b30cbc0cd1a4
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-08-23 17:28:06 +0200

    adding the variance in the fitted line by lsq

--- src/hypton/effective_hyperuniform.py
+++ src/hypton/effective_hyperuniform.py
@@ -1,8 +1,10 @@
 import numpy as np
+from scipy.linalg.misc import norm
 from scipy.signal import find_peaks
 from hypton.utils import (
     _binning_function,
 )
+import scipy.optimize as optimization
 
 
 class EffectiveHyperuniform:
@@ -14,25 +16,40 @@ class EffectiveHyperuniform:
         self.norm_k = norm_k
         self.sf = sf
         self.fitted_line = None
+        self.i_first_peak = None
 
     def bin_data(self, **params):
         # ! what is the purpose of this method ?
         return _binning_function(self.norm_k.ravel(), self.sf.ravel(), **params)
 
-    def index_H(self, norm_k, sf, norm_k_stop=None):
+    def index_H(self, norm_k, sf, std=None, norm_k_stop=None):
+
         if norm_k_stop is not None:
             norm_k_list = list(norm_k.ravel())
             index = min(
                 range(len(norm_k_list)), key=lambda i: abs(norm_k_list[i] - norm_k_stop)
             )  # index of the closest value to k_stop in norm_k
         i = len(norm_k) if norm_k_stop is None else index
-        fitting_param = np.polyfit(norm_k[:i], sf[:i], deg=1)
-        S_0 = fitting_param[-1]
+        poly = lambda x, a, b: a + b * x
+        if std is not None:
+            fitting_params, fitting_cov = optimization.curve_fit(
+                f=poly, xdata=norm_k[:i], ydata=sf[:i], sigma=std[:i]
+            )
+        else:
+            fitting_params, fitting_cov = optimization.curve_fit(
+                f=poly, xdata=norm_k[:i], ydata=sf[:i]
+            )
+        std_intercept = np.sqrt(np.diag(fitting_cov))[0]
+        S_0 = fitting_params[0]
+        print(fitting_params[1])
+        self.fitted_line = lambda x: fitting_params[1] * x + S_0
         thresh = 1
-        peak, _ = find_peaks(sf, height=1)
-        if list(peak):
-            S_first_peak = max(peak[0], 1)
+        i_peak, _ = find_peaks(sf, height=thresh)
+
+        if list(i_peak):
+            self.i_first_peak = max(i_peak[0], 1)
+            S_first_peak = sf[self.i_first_peak]
         else:
             S_first_peak = 1
-        self.fitted_line = lambda x: fitting_param[0] * x + S_0
-        return S_0 / S_first_peak
+
+        return S_0 / S_first_peak, std_intercept
