1654eb242f3cad2d16758c55dad90f10a715c5ed
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-09-20 10:55:55 +0200

    update allowed values test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,10 +5,13 @@ import hypton.utils as utils
 
 @pytest.mark.parametrize(
     "L, max_k, meshgrid_size",
-    [(2 * np.pi, 4, 2 * 4 + 1)],
+    [
+        (2 * np.pi, 4, 2 * 4 + 1),
+        (2 * np.pi, 10, 2 * 10 + 1),
+    ],
 )
 def test_allowed_wave_values(L, max_k, meshgrid_size):
-    x_k = np.arange(-max_k, max_k, 1)
+    x_k = np.arange(-max_k, max_k + 1, 1)
     x_k = x_k[x_k != 0]
     X, Y = np.meshgrid(x_k, x_k)
     true_allowed_wave_values = 2 * np.pi * np.column_stack((X.ravel(), Y.ravel())) / L
