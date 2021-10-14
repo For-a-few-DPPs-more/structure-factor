49e423e36cf5574aac9527dceaf8c7142cea49ce
Guillaume Gautier <guillaume.gga@gmail.com>
2021-10-14 10:37:33 +0200

    test point process: estimate intensity

--- src/hypton/homogeneous_poisson_process.py
+++ src/hypton/homogeneous_poisson_process.py
@@ -3,7 +3,7 @@ from hypton.utils import get_random_number_generator
 
 
 class HomogeneousPoissonPointProcess(object):
-    """`Homogeneous Poisson Point Process <https://en.wikipedia.org/wiki/Poisson_point_process#Spatial_Poisson_point_process>`_"""
+    """`Homogeneous Poisson point process <https://en.wikipedia.org/wiki/Poisson_point_process#Spatial_Poisson_point_process>`_"""
 
     def __init__(self, intensity=1.0):
         """Create a `homogeneous Poisson point process <https://en.wikipedia.org/wiki/Poisson_point_process#Spatial_Poisson_point_process>`_ with prescribed (positive) ``intensity`` parameter.
@@ -15,18 +15,18 @@ class HomogeneousPoissonPointProcess(object):
             raise TypeError("intensity argument must be positive")
         self.intensity = intensity
 
-    def generate_sample(self, window=UnitBoxWindow(2), random_state=None):
+    def generate_sample(self, window=UnitBoxWindow(2), seed=None):
         r"""Generate an exact sample from the corresponding :py:class:`HomogeneousPoissonPointProcess` restricted to the :math:`d` dimensional `window`.
 
         Args:
-            window (AbstractSpatialWindow, optional):. Defaults to ``UnitBoxWindow(2)`` :math:`[0,1]^2`.
+            window (AbstractSpatialWindow, optional):. Defaults to ``UnitBoxWindow(2)`` :math:`=[0,1]^2`.
 
         Returns:
-            points: numpy.ndarray of size :math:`n \times d`, where :math:`n` is the number of points forming the sample.
+            numpy.ndarray: of size :math:`n \times d`, where :math:`n` is the number of points forming the sample.
         """
         if not isinstance(window, AbstractSpatialWindow):
             raise TypeError("window argument must be an AbstractSpatialWindow")
 
-        rng = get_random_number_generator(random_state)
+        rng = get_random_number_generator(seed)
         nb_points = rng.poisson(self.intensity * window.volume)
-        return window.rand(nb_points, random_state=rng)
+        return window.rand(nb_points, seed=rng)
