188027fa3e11af0f0b51631a41409acf842ea03f
Guillaume Gautier <guillaume.gga@gmail.com>
2021-10-20 15:17:42 +0200

    update docstring and test of spatial windows

--- src/structure_factor/spatial_windows.py
+++ src/structure_factor/spatial_windows.py
@@ -38,15 +38,19 @@ class AbstractSpatialWindow(metaclass=ABCMeta):
 
     @abstractmethod
     def __contains__(self, point):
-        """Return True if ``point`` falls inside the corresponding window, otherwise return False."""
+        """Return True if :math:`d` dimensional vector ``point`` falls inside the corresponding :math:`d` dimensional window, otherwise return False.
+
+        Args:
+            point (numpy.ndarray): :math:`d` dimensional vector to test.
+        """
 
     def indicator_function(self, points):
         r"""Return the indicator function of the corresponding window evaluated at each of the :math:`n` ``points``.
 
         Args:
-            points (np.ndarray): vector of size :math:`d` or array of size :math:`n \times d` containing point(s) to be tested.
+            points (numpy.ndarray): vector of size :math:`d` or array of size :math:`n \times d` containing point(s) to be tested.
         Returns:
-            bool or np.ndarray:
+            bool or numpy.ndarray:
             - If :math:`n=1`, bool,
             - If :math:`n>1`, :math:`n` dimensional boolean array.
         """
@@ -60,10 +64,10 @@ class AbstractSpatialWindow(metaclass=ABCMeta):
 
         Args:
             n (int, optional): Number of points. Defaults to 1.
-            seed (optional): Defaults to None.
+            seed (int or np.random.Generator, optional): Defaults to None.
 
         Returns:
-            np.ndarray:
+            numpy.ndarray:
             - If :math:`n=1`, :math:`d` dimensional vector
             - If :math:`n>1`, :math:`n \times d` array containing the points.
         """
@@ -81,7 +85,7 @@ class BallWindow(AbstractSpatialWindow):
         """
         _center = np.array(center)
         if not _center.ndim == 1:
-            raise ValueError("center must be 1D np.ndarray")
+            raise ValueError("center must be 1D numpy.ndarray")
         if not radius > 0:
             raise ValueError("radius must be positive")
         self.center = _center
@@ -145,6 +149,11 @@ class UnitBallWindow(BallWindow):
     """
 
     def __init__(self, center):
+        """Initialize unit ball window from ``center``.
+
+        Args:
+            center (numpy.ndarray): center of the ball.
+        """
         super().__init__(center, radius=1.0)
 
 
@@ -152,18 +161,18 @@ class BoxWindow(AbstractSpatialWindow):
     r"""Create a :math:`d` dimensional box window :math:`\prod_{i=1}^{d} [a_i, b_i]` from ``bounds[i, :]`` :math:`=[a_i, b_i]`."""
 
     def __init__(self, bounds):
-        r"""Create an instance of BoxWindow.
+        r"""Initialize BoxWindow from ``bounds[i, :]`` :math:`=[a_i, b_i]`.
 
         Args:
             bounds (numpy.ndarray): :math:`d \times 2` array describing the bounds of the box row-wise.
         """
         _bounds = np.atleast_2d(bounds)
         if _bounds.ndim != 2:
-            raise ValueError("bounds must be 2d np.ndarray")
+            raise ValueError("bounds must be 2d numpy.ndarray")
         if _bounds.shape[1] != 2:
-            raise ValueError("bounds must be d x 2 np.ndarray")
+            raise ValueError("bounds must be d x 2 numpy.ndarray")
         if np.any(np.diff(_bounds, axis=-1) <= 0):
-            raise ValueError("all bounds [a_i, b_i] must satisfy b_i > a_i")
+            raise ValueError("all bounds [a_i, b_i] must satisfy a_i < b_i")
         # use transpose to facilitate operations (unpacking, diff, rand, etc)
         self._bounds = np.transpose(_bounds)
 
@@ -184,8 +193,9 @@ class BoxWindow(AbstractSpatialWindow):
         return np.prod(np.diff(self._bounds, axis=0))
 
     def __contains__(self, point):
-        assert point.ndim == 1 and point.size == self.dimension
-        return self.indicator_function(point)
+        _point = np.array(point)
+        assert _point.ndim == 1 and _point.size == self.dimension
+        return self.indicator_function(_point)
 
     def indicator_function(self, points):
         a, b = self._bounds
@@ -195,8 +205,9 @@ class BoxWindow(AbstractSpatialWindow):
 
     def rand(self, n=1, seed=None):
         rng = get_random_number_generator(seed)
-        d = self.dimension
-        return rng.uniform(*self._bounds, size=(d,) if n == 1 else (n, d))
+        a, b = self._bounds
+        dim = self.dimension
+        return rng.uniform(a, b, size=(dim,) if n == 1 else (n, dim))
 
     def to_spatstat_owin(self, **params):
         """Convert the object to a ``spatstat.geom.owin`` R object of type  ``owin``.
--- tests/test_spatial_windows.py
+++ tests/test_spatial_windows.py
@@ -1,27 +1,60 @@
 import numpy as np
 import pytest
 
-from structure_factor.spatial_windows import BallWindow, BoxWindow, UnitBoxWindow
+from structure_factor.spatial_windows import (
+    BallWindow,
+    BoxWindow,
+    UnitBallWindow,
+    UnitBoxWindow,
+)
 from structure_factor.utils import get_random_number_generator
 
 ##### BallWindow
 
+VOLUME_UNIT_BALL = {
+    1: 2,
+    2: np.pi,
+    3: 4 * np.pi / 3,
+    4: np.pi ** 2 / 2,
+    5: 8 * np.pi ** 2 / 15,
+    6: np.pi ** 3 / 6,
+}
+
 
 @pytest.mark.parametrize(
-    "dimension, radius, factor",
+    "dimension, seed",
     (
-        (1, 10, 2),
-        (2, 10, np.pi),
-        (3, 10, 4 * np.pi / 3),
-        (4, 10, np.pi ** 2 / 2),
-        (5, 10, 8 * np.pi ** 2 / 15),
-        (6, 10, np.pi ** 3 / 6),
+        (1, None),
+        (2, None),
+        (3, None),
+        (4, None),
+        (5, None),
+        (6, None),
     ),
 )
-def test_volume_ball(dimension, radius, factor):
-    center = np.zeros(dimension)
+def test_volume_unit_ball(dimension, seed):
+    rng = get_random_number_generator(seed)
+    center = rng.normal(size=dimension)
+    ball = UnitBallWindow(center)
+    np.testing.assert_almost_equal(ball.volume, VOLUME_UNIT_BALL[dimension])
+
+
+@pytest.mark.parametrize(
+    "dimension, radius, seed",
+    (
+        (1, 10, None),
+        (2, 10, None),
+        (3, 10, None),
+        (4, 10, None),
+        (5, 10, None),
+        (6, 10, None),
+    ),
+)
+def test_volume_ball(dimension, radius, seed):
+    rng = get_random_number_generator(seed)
+    center = rng.normal(size=dimension)
     ball = BallWindow(center, radius)
-    expected = factor * radius ** dimension
+    expected = VOLUME_UNIT_BALL[dimension] * radius ** dimension
     np.testing.assert_almost_equal(ball.volume, expected)
 
 
@@ -35,7 +68,7 @@ def test_volume_ball(dimension, radius, factor):
         [100, None],
     ),
 )
-def test_center_belongs_to_unit_ball(dimension, seed):
+def test_center_belongs_to_ball(dimension, seed):
     rng = get_random_number_generator(seed)
     center = rng.normal(size=dimension)
     ball = BallWindow(center)
@@ -92,20 +125,24 @@ def test_volume_unit_box_is_one(dimension, seed):
 @pytest.mark.parametrize(
     "point, expected",
     [
-        (np.array([0, 0]), True),
-        (np.array([-1, 5]), True),
-        (np.array([5, 6]), False),
+        ([0, 0], True),
+        ([-5, -5], True),
+        ([-5, 5], True),
+        ([5, -5], True),
+        ([5, 5], True),
+        ([-1, 5], True),
+        ([5, 6], False),
     ],
 )
 def test_box_2d_contains_point(point, expected):
-    bounds = np.array([[-5, 5], [-5, 5]])
+    bounds = [[-5, 5], [-5, 5]]
     box = BoxWindow(bounds)
     assert (point in box) == expected
 
 
 @pytest.mark.parametrize("nb_points", [1, 100])
 def test_random_points_fall_inside_box(nb_points):
-    bounds = np.array([[-5, 5], [-5, 5]])
+    bounds = [[-5, 5], [-5, 5]]
     box = BoxWindow(bounds)
     random_points = box.rand(nb_points)
     indicator_vector = box.indicator_function(random_points)
