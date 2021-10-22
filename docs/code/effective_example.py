32e4c18a4b0de89d5704625c1276632dec0ed306
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-10-22 19:14:07 +0200

    update name with the last change

--- docs/code/effective_example.py
+++ docs/code/effective_example.py
@@ -15,18 +15,18 @@ window = BoxWindow(bounds)  # create a cubic window
 ginibre_pp_box = ginibre_pp.restrict_to_window(window)
 # scattering intensity
 sf_ginibre_box = StructureFactor(ginibre_pp_box)  # initialize the class StructureFactor
-norm_k, si = sf_ginibre_box.scattering_intensity(k_component_max=6, meshgrid_shape=200)
+k_norm, si = sf_ginibre_box.scattering_intensity(k_max=6, meshgrid_shape=200)
 
 
 # test effective hyperuniformity
 from structure_factor.hyperuniformity import Hyperuniformity
 
 # initialize Hyperuniformity
-hyperuniformity_test = Hyperuniformity(norm_k, si)
+hyperuniformity_test = Hyperuniformity(k_norm, si)
 # regularization of the approximated result
 hyperuniformity_test.bin_data(bins=40)
 # find the H index
-H_ginibre, std = hyperuniformity_test.index_H(norm_k_stop=4)
+H_ginibre, std = hyperuniformity_test.index_H(k_norm_stop=4)
 print("H_ginibre=", H_ginibre)
 
 
@@ -38,16 +38,16 @@ fitted_sf_line = hyperuniformity_test.fitted_line  # fittend ligne
 index_peak = hyperuniformity_test.i_first_peak
 
 
-mean_norm_k = hyperuniformity_test.norm_k
+mean_k_norm = hyperuniformity_test.k_norm
 mean_sf = hyperuniformity_test.sf
 x = np.linspace(0, 5, 300)
 y = np.linspace(0, 15, 500)
 fig = plt.figure(figsize=(10, 6))
-plt.plot(mean_norm_k, mean_sf, "b.", label="approx_sf")
-plt.plot(mean_norm_k, mean_sf, "b", label="approx_sf")
+plt.plot(mean_k_norm, mean_sf, "b.", label="approx_sf")
+plt.plot(mean_k_norm, mean_sf, "b", label="approx_sf")
 plt.plot(x, fitted_sf_line(x), "r--", label="fitted line")
 plt.plot(y, utils.structure_factor_ginibre(y), "g", label="exact sf")
-plt.plot(mean_norm_k[index_peak], mean_sf[index_peak], "k*", label="first peak")
+plt.plot(mean_k_norm[index_peak], mean_sf[index_peak], "k*", label="first peak")
 plt.legend()
 plt.xlabel("wavelength ($||\mathbf{k}||$)")
 plt.ylabel("Structure factor ($\mathsf{S}(\mathbf{k})$)")
