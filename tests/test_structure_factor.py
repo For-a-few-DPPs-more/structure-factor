e1348466ba49c1d152997afc7e0b74fb9f61d80f
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-10-28 12:10:11 +0200

    add test scattering intensity

--- tests/test_structure_factor.py
+++ tests/test_structure_factor.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from structure_factor.data import load_data
+from structure_factor.spatial_windows import BoxWindow
 from structure_factor.structure_factor import StructureFactor
 from structure_factor.utils import (
     pair_correlation_function_ginibre,
@@ -22,6 +23,60 @@ def k_norm():
 @pytest.fixture
 def ginibre_pp():
     return load_data.load_ginibre()
+
+
+def test_scattering_intensity(ginibre_pp):
+    L = ginibre_pp.window.radius / np.sqrt(2)  # sidelength of the cubic window
+    bounds = [[-L / 2, L / 2], [-L / 2, L / 2]]
+    window = BoxWindow(bounds)  # create a cubic window
+    ginibre_pp_box = ginibre_pp.restrict_to_window(window)
+    sf_pp = StructureFactor(ginibre_pp_box)
+    k_norm, si = sf_pp.scattering_intensity(
+        k_max=1,
+        meshgrid_shape=(4, 4),
+    )
+    expected_k_norm = np.array(
+        [
+            1.38230077,
+            1.04005223,
+            1.01313319,
+            1.38230077,
+            1.04005223,
+            0.50265482,
+            0.44428829,
+            1.04005223,
+            1.01313319,
+            0.44428829,
+            0.37699112,
+            1.01313319,
+            1.38230077,
+            1.04005223,
+            1.01313319,
+            1.38230077,
+        ]
+    )
+    expected_si = np.array(
+        [
+            0.13647604,
+            0.32944414,
+            0.28438074,
+            1.23781715,
+            0.25049137,
+            0.27434747,
+            0.11169433,
+            0.05213884,
+            0.07547196,
+            0.02700638,
+            0.01302653,
+            0.27565412,
+            1.23781715,
+            0.07421566,
+            0.02024202,
+            0.13647604,
+        ]
+    )
+    np.testing.assert_almost_equal(k_norm, expected_k_norm)
+    np.testing.assert_almost_equal(si, expected_si)
 
 
 def test_interpolate_pcf_ginibre(ginibre_pp, radius):
