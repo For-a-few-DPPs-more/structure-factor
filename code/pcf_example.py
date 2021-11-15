0f5365ec1fbde7d202fd5492005b3ae7b53081eb
guilgautier <guillaume.gga@gmail.com>
2021-11-15 16:41:02 +0000

    Update gh-pages to output generated at 6af7c59

--- code/pcf_example.py
+++ code/pcf_example.py
@@ -1,20 +1,23 @@
+import structure_factor.utils as utils
 from structure_factor.data import load_data
 from structure_factor.structure_factor import StructureFactor
-import structure_factor.utils as utils
-
 
 # load Ginibre PointPattern
 ginibre_pp = load_data.load_ginibre()
 
 # initialize the class StructureFactor
-sf_ginibre = StructureFactor(ginibre_pp)  
+sf_ginibre = StructureFactor(ginibre_pp)
 
 # compute the pair correlation function
-pcf_fv = sf_ginibre.compute_pcf(method="fv", Kest=dict(rmax=45), fv=dict(method="b", spar=0.1))
+pcf_fv = sf_ginibre.compute_pcf(
+    method="fv", Kest=dict(rmax=45), fv=dict(method="b", spar=0.1)
+)
 
 # plot
-sf_ginibre.plot_pcf(pcf_fv, 
-                    exact_pcf=utils.pair_correlation_function_ginibre,
-                    figsize=(10,6),
-                    color=[ 'grey','b', 'darkcyan'],
-                    style=[".", "o", "^"])
+sf_ginibre.plot_pcf(
+    pcf_fv,
+    exact_pcf=utils.pair_correlation_function_ginibre,
+    figsize=(10, 6),
+    color=["grey", "b", "darkcyan"],
+    style=[".", "o", "^"],
+)
