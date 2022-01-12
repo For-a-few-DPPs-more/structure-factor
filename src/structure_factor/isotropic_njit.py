4954aaed0509a901b4c3a9f8f683c48eca6d5def
hawat629 <58569872+hawat629@users.noreply.github.com>
2022-01-12 08:28:22 +0100

    isotropic_njit: unsignificant changes, merge all fct used in isotropic case

--- src/structure_factor/isotropic_njit.py
+++ src/structure_factor/isotropic_njit.py
@@ -53,3 +53,40 @@ def isotropic_estimator_njit(result, norm_k, norm_xi_xj, order):
                 tmp /= k_xi_xj
             sum_k += tmp
         result[k] = 2 * sum_k
+
+
+# old version without numba
+def select_periodogram_isotropic(debiased, direct):
+    return isotropic_bartlett_estimator
+
+
+def isotropic_bartlett_estimator(k_norm, point_pattern, taper=None):
+    # ! the current implem may take some time when there are a lot of wave vectors and points
+    window = point_pattern.window
+    #! fi window doenst exists ?
+    d = window.dimension
+    unit_ball = UnitBallWindow(np.zeros(d))
+
+    X = np.atleast_2d(point_pattern.points)
+
+    norm_x_y = pdist(X, metric="euclidean")  # ||x_i - x_j|| for i < j
+
+    k_xy = np.multiply.outer(k_norm, norm_x_y)
+    order = d / 2 - 1
+    J_k_xy = bessel1(order, k_xy)
+
+    estimated_sf_k = np.zeros_like(k_norm)
+    # for i, k_ in enumerate(k_norm):
+    #     estimated_sf_k[i] = bessel1(order, k_ * norm_x_y).sum()
+    if order > 0:
+        np.power(k_xy, order, out=k_xy)
+        J_k_xy /= k_xy
+    np.sum(J_k_xy, axis=1, out=estimated_sf_k)
+    estimated_sf_k *= 2  # sum_{i neq j} = 2 sum_{i<j}
+
+    surface, volume = unit_ball.surface, window.volume
+    rho = point_pattern.intensity
+    estimated_sf_k *= (2 * np.pi) ** (d / 2) / (surface * volume * rho)
+    estimated_sf_k += 1
+
+    return k_norm, estimated_sf_k
