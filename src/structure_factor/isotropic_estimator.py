9085dbf7a4bf68145ff39a3d80b813719c6fd0d8
hawat629 <58569872+hawat629@users.noreply.github.com>
2022-01-17 18:28:08 +0100

    isotropic_estimator: rename, refactor

---
+++ src/structure_factor/isotropic_estimator.py
@@ -0,0 +1,55 @@
+import numba_scipy
+import numba_scipy.special
+import numpy as np
+import scipy.special as sc
+from numba import njit
+from scipy.spatial.distance import pdist
+
+from structure_factor.spatial_windows import UnitBallWindow
+
+
+def bartlett_estimator(point_pattern, k_norm=None, n_allowed_k_norm=100):
+    window = point_pattern.window
+    d = window.dimension
+    unit_ball = UnitBallWindow(np.zeros(d))
+
+    X = np.atleast_2d(point_pattern.points)
+    norm_xi_xj = pdist(X, metric="euclidean")
+
+    # allowed wavenumbers
+    if k_norm == None:
+        k_norm = sc.jn_zeros(d / 2, n_allowed_k_norm) / window.radius
+
+    estimator = np.zeros_like(k_norm)
+    order = float(d / 2 - 1)
+    isotropic_estimator_njit(estimator, k_norm, norm_xi_xj, order)
+
+    surface, volume = unit_ball.surface, window.volume
+    rho = point_pattern.intensity
+    estimator *= (2 * np.pi) ** (d / 2) / (surface * volume * rho)
+    estimator += 1
+
+    return k_norm, estimator
+
+
+@njit("double(double, double)")
+def bessel1_njit(order, x):
+    if order == 0.0:
+        return sc.j0(x)
+    if order == 1.0:
+        return sc.j1(x)
+    return sc.jv(order, x)
+
+
+@njit("void(double[:], double[:], double[:], double)")
+def isotropic_estimator_njit(result, k_norm, norm_xi_xj, order):
+    for k in range(len(k_norm)):
+        sum_k = 0.0
+        for ij in range(len(norm_xi_xj)):
+            k_xi_xj = k_norm[k] * norm_xi_xj[ij]
+            tmp = bessel1_njit(order, k_xi_xj)
+            if order > 0:
+                k_xi_xj = k_xi_xj ** order
+                tmp /= k_xi_xj
+            sum_k += tmp
+        result[k] = 2 * sum_k
