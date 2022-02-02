d363872c68efbd5f39fa6168ef0a3b700591fc8d
hawat629 <58569872+hawat629@users.noreply.github.com>
2022-02-02 22:34:07 +0100

    refactor test structure_factor

--- tests/test_structure_factor.py
+++ tests/test_structure_factor.py
@@ -20,119 +20,150 @@ def ginibre_pp():
 
 
 # todo reshape the test, scattering_intensity does not return k_norm but k
-#
-def test_scattering_intensity(ginibre_pp):
+@pytest.mark.parametrize(
+    "to_test",
+    ["k_norm", "s_si"],
+)
+def test_scattering_intensity_of_ginibre_on_allowed_values(ginibre_pp, to_test):
     """Test scattering intensity of the Ginibre on allowed wavevectors"""
-    L = ginibre_pp.window.radius / np.sqrt(2)  # sidelength of the cubic window
+    # PointPattern
+    point_pattern = ginibre_pp
+
+    # Restric to BoxWindpw
+    L = point_pattern.window.radius / np.sqrt(2)  # sidelength of the cubic window
     bounds = [[-L / 2, L / 2], [-L / 2, L / 2]]
     window = BoxWindow(bounds)  # create a cubic window
-    ginibre_pp_box = ginibre_pp.restrict_to_window(window)
+    ginibre_pp_box = point_pattern.restrict_to_window(window)
+
+    # Initialize StructureFactor
     sf_pp = StructureFactor(ginibre_pp_box)
-    k, si = sf_pp.scattering_intensity(
+    k, s_si = sf_pp.scattering_intensity(
         k_max=1,
         meshgrid_shape=(2, 3),
     )
-    k_norm = utils.norm_k(k)
-    expected_k_norm = np.array(
-        [
-            1.38230077,
-            1.38230077,
-            1.04005223,
-            1.04005223,
-            1.01313319,
-            1.01313319,
-            1.38230077,
-            1.38230077,
-        ]
-    )
-    expected_si = np.array(
-        [
-            0.13642892,
-            1.23738984,
-            0.2504049,
-            0.05212084,
-            0.07544591,
-            0.27555896,
-            1.23738984,
-            0.13642892,
-        ]
-    )
-    np.testing.assert_almost_equal(k_norm, expected_k_norm)
-    np.testing.assert_almost_equal(si, expected_si)
+    if to_test == "k_norm":
+        expected = utils.norm_k(k)
+        result = np.array(
+            [
+                1.38230077,
+                1.38230077,
+                1.04005223,
+                1.04005223,
+                1.01313319,
+                1.01313319,
+                1.38230077,
+                1.38230077,
+            ]
+        )
+    elif to_test == "s_si":
+        expected = s_si
+        result = np.array(
+            [
+                0.13642892,
+                1.23738984,
+                0.2504049,
+                0.05212084,
+                0.07544591,
+                0.27555896,
+                1.23738984,
+                0.13642892,
+            ]
+        )
+
+    np.testing.assert_almost_equal(expected, result)
 
 
 @pytest.mark.parametrize(
-    "k, points, unscaled_expected",
-    [
-        (np.array([[1], [2]]), np.array([[np.pi], [2 * np.pi]]), np.array([0, 4])),
-    ],
+    " debiased, direct, estimator",
+    [(False, False, "si"), (True, True, "si_ddtp"), (True, False, "si_udtp")],
 )
-def test_scattering_intensity2(k, points, unscaled_expected):
+def test_scattering_intensity_on_specific_points_and_wavevector(
+    debiased, direct, estimator
+):
     """Test the scattering intensity and the debiased versions on simple choice of wavevectors in 1-d, and specific points allowing to simplify the calculation"""
-    window = BoxWindow([-8, 8])
+    # BoxWindow
+    L = 8
+    window = BoxWindow([-L, L])
+
+    # PointPattern
+    points = np.array([[np.pi], [2 * np.pi]])
     point_pattern = PointPattern(points, window)
+
+    # StructureFactor
     sf = StructureFactor(point_pattern)
-    intensity = point_pattern.intensity
-    _, si = sf.scattering_intensity(k, debiased=False)
-    expected_si = unscaled_expected / (window.volume * intensity)
+    rho = point_pattern.intensity
 
-    _, si_ddtp = sf.scattering_intensity(k, debiased=True, direct=True)
-    expected_si_ddtp = (
-        1
-        / intensity
-        * (
-            np.sqrt(unscaled_expected / window.volume)
-            - intensity * BartlettTaper.ft_taper(k, window)
+    # Scattering intensity
+    k = np.array([[1], [2]])
+    _, result = sf.scattering_intensity(k, debiased=debiased, direct=direct)
+
+    # Expected
+    unscaled_expected = np.array([0, 4])
+    if estimator == "si":
+        expected = unscaled_expected / (window.volume * rho)
+    elif estimator == "si_ddtp":
+        expected = (
+            1
+            / rho
+            * (
+                np.sqrt(unscaled_expected / window.volume)
+                - rho * BartlettTaper.ft_taper(k, window)
+            )
+            ** 2
         )
-        ** 2
-    )
-
-    _, si_udtp = sf.scattering_intensity(k, debiased=True, direct=False)
-    expected_si_udtp = expected_si - intensity * BartlettTaper.ft_taper(k, window) ** 2
-    np.testing.assert_almost_equal(si, expected_si)
-    np.testing.assert_almost_equal(si_ddtp, expected_si_ddtp)
-    np.testing.assert_almost_equal(si_udtp, expected_si_udtp)
+    elif estimator == "si_udtp":
+        expected = (
+            unscaled_expected / (window.volume * rho)
+            - rho * BartlettTaper.ft_taper(k, window) ** 2
+        )
+    np.testing.assert_almost_equal(result, expected)
 
 
-def test_tapered_periodogram():
+@pytest.mark.parametrize(
+    "debiased, direct",
+    [(False, False), (True, True), (True, False)],
+)
+def test_tapered_periodogram_with_bartlett_taper_equal_scattering_intensity(
+    debiased, direct
+):
     r"""Test that the debiased versions of :math:`\widehat{S}_{\mathrm{TP}}` with Bartlett taper gave the scattering intensity and the debiased verions."""
-    points = np.random.rand(20, 3) * 5
-    window = BoxWindow([[-6, 6], [-7, 5], [-5, 6]])
-    taper = BartlettTaper
 
+    # PointPattern
+    window = BoxWindow([[-6, 6], [-7, 5], [-5, 6]])
+    points = window.rand(20)
     point_pattern = PointPattern(points, window)
+
+    # StructureFactor
     sf = StructureFactor(point_pattern)
     k = np.random.randn(10, 3)
+    taper = BartlettTaper
 
-    # scattering intensity on k
-    _, si_k = sf.scattering_intensity(k, debiased=False)
-    s_tp = sf.tapered_periodogram(k, taper, debiased=False)
-    # directly debiased scattering intensity
-    _, si_dd = sf.scattering_intensity(k, debiased=True, direct=True)
-    s_ddtp = sf.tapered_periodogram(k, taper, debiased=True, direct=True)
-    # undirectly debiased scattering intensity
-    _, si_ud = sf.scattering_intensity(k, debiased=True, direct=False)
-    s_udtp = sf.tapered_periodogram(k, taper, debiased=True, direct=False)
+    # Scattering intensity and tapered periodogram
+    _, s_si = sf.scattering_intensity(k, debiased=debiased, direct=direct)
+    s_tp = sf.tapered_periodogram(k, taper, debiased=debiased, direct=direct)
 
-    np.testing.assert_almost_equal(si_k, s_tp)
-    np.testing.assert_almost_equal(si_dd, s_ddtp)
-    np.testing.assert_almost_equal(si_ud, s_udtp)
+    np.testing.assert_almost_equal(s_si, s_tp)
 
 
-def test_bartlett_isotropic_estimator():
+def test_bartlett_isotropic_estimator_on_origin():
     r"""Test the estimator :math:`\widehat{S}_{\mathrm{BI}}`, for simple choice of parameters in 3-d"""
-    points = np.random.rand(2, 3) * 3
-    d = points.shape[1]
+    # window
     r = 4
     window = BallWindow(center=[0, 0, 0], radius=r)
-    window_volume = 4 / 3 * np.pi * r ** 3
-    window_surface = 4 * np.pi
+    points = window.rand(2)
+
+    # PointPattern
     point_pattern = PointPattern(points, window)
     k_norm = np.array([1.0])
+
     # s_bi
     sf = StructureFactor(point_pattern)
     _, s_bi = sf.bartlett_isotropic_estimator(k_norm)
-    # expected s_bi
+
+    # Expected s_bi
+    d = points.shape[1]
+    window_volume = 4 / 3 * np.pi * r ** 3
+    window_surface = 4 * np.pi
     dist_points = np.linalg.norm(points[0] - points[1])
     m = (2 * np.pi) ** (d / 2) / (
         point_pattern.intensity * window_volume * window_surface
@@ -164,6 +195,3 @@ def test_compute_structure_factor_ginibre_with_baddour_chouinard(ginibre_pp):
     )
     sf_expected = structure_factor_ginibre(k_norm)
     np.testing.assert_almost_equal(sf_computed, sf_expected)
-    # fig = sf_pp.plot_sf_hankel_quadrature(
-    #     k_norm, sf_computed, exact_sf=utils.structure_factor_ginibre
-    # )
