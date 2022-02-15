c57ddb9a5fe35d7b23b42275d08819712bb5ad71
hawat629 <58569872+hawat629@users.noreply.github.com>
2022-02-15 12:08:18 +0100

    docs: code for isotropic_estimator

---
+++ docs/code/isotropic_estimator/allowed_k_norm.py
@@ -0,0 +1,3 @@
+from structure_factor.isotropic_estimator import allowed_k_norm
+k = allowed_k_norm(d=2, r=20, n=6)
+print(k)
---
+++ docs/code/isotropic_estimator/bartlett_estimator.py
@@ -0,0 +1,15 @@
+# Generate a PointPattern in a BallWindow
+from structure_factor.point_process import HomogeneousPoissonPointProcess
+from structure_factor.spatial_windows import BallWindow
+from structure_factor.point_pattern import PointPattern
+import numpy as np
+
+poisson = HomogeneousPoissonPointProcess(intensity=1/(4*np.pi) ) # Initialize a Poisson point process
+window = BallWindow(radius=20, center=[0,0,0])
+poisson_points = poisson.generate_sample(window=window)  # Sample a realization
+poisson_pp = PointPattern(points=poisson_points, window=window)  # Point pattern
+
+# Bartlett's estimator
+from structure_factor.isotropic_estimator import bartlett_estimator
+k_norm = np.linspace(1, 5, 3) # wavenumbers
+k_norm, estimation = bartlett_estimator(poisson_pp, k_norm=k_norm)
