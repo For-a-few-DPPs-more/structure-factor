dd5fd9f636fd45a06c32163047e07ef2a2bda363
hawat629 <58569872+hawat629@users.noreply.github.com>
2022-02-17 20:53:51 +0100

    taper example

---
+++ docs/code/structure_factor/taper.py
@@ -0,0 +1,15 @@
+class MyTaper:
+
+    @staticmethod
+    def taper(x, window):
+        """A taper is a function supported on a window.
+        """
+        taper = ... # The taper
+        return taper
+
+    @staticmethod
+    def ft_taper(k, window):
+        """The Fourier transform of the taper.
+        """
+        ft = ... # Fourier transform of the taper
+        return ft 
