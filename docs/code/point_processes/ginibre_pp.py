48e1f63c01f968e734dd7dc6beaeffec16b1fbde
Guillaume Gautier <guillaume.gga@gmail.com>
2022-02-22 10:44:48 +0100

    docs: remove obsolete code files

--- docs/code/point_processes/ginibre_pp.py
+++ docs/code/point_processes/ginibre_pp.py
@@ -2,7 +2,7 @@
 from structure_factor.point_processes import GinibrePointProcess
 from structure_factor.spatial_windows import BallWindow
 
-window = BallWindow(center=[0,0], radius=40) # Observation window
-ginibre = GinibrePointProcess() # Ginibre process
-ginibre_pp = ginibre.generate_point_pattern(window=window) # PointPattern
-ginibre_pp.plot(file_name="ginibre_pp.pdf") # Plot and save the figure
+window = BallWindow(center=[0, 0], radius=40)  # Observation window
+ginibre = GinibrePointProcess()  # Ginibre process
+ginibre_pp = ginibre.generate_point_pattern(window=window)  # PointPattern
+ginibre_pp.plot()  # Plot and save the figure
