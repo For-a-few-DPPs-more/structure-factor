5d9e42a51667d8f26b803a9ee2e5cd5f34756cd8
hawat629 <58569872+hawat629@users.noreply.github.com>
2023-02-23 12:26:55 +0100

    remove package spatstat.model, and return update to false

--- src/structure_factor/__init__.py
+++ src/structure_factor/__init__.py
@@ -1,4 +1,4 @@
 # from spatstat_interface.interface import SpatstatInterface
 
 # spatstat = SpatstatInterface(update=False)
-# spatstat.import_package("model", "explore", "geom", update=True)
+# spatstat.import_package("explore", "geom", update=False)
