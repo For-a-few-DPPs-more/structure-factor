b8bfd685f3ae4d4745c8f27e2773b74840981615
hawat629 <58569872+hawat629@users.noreply.github.com>
2023-02-23 11:38:42 +0100

    update init: replace core by model and explore with update=true

--- src/structure_factor/__init__.py
+++ src/structure_factor/__init__.py
@@ -1,4 +1,4 @@
 # from spatstat_interface.interface import SpatstatInterface
 
 # spatstat = SpatstatInterface(update=False)
-# spatstat.import_package("core", "geom", update=False)
+# spatstat.import_package("model", "explore", "geom", update=True)
